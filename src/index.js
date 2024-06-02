310834b458aedbead5df14aad04b42129bdc2b27
Deniss <karamulkin@gmail.com>
2024-06-02 11:13:47 +0300

    Routes corrected

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './components/App';
 import StorePage from './components/StorePage';
 import SingleProductPage from './components/SingleProductPage';
