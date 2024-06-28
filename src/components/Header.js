97d4e61055413a3a67230f2f1b817dbee1294fde
Deniss <karamulkin@gmail.com>
2024-06-28 23:39:03 +0300

    nav buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,9 +14,9 @@ export const Header  = () => {
             <img src={logo} alt ="Logo" className={styles.logo} />
             <nav className={styles.nav}>
                 <ul className={styles.navUl}>
-                    <li className={styles.navLi}><Link to="/" className="home">Home</Link></li>
-                    <li className={styles.navLi}><Link to="/store" className="store">Store</Link></li>
-                    <li className={styles.navLi}><Link to="/cart" className="cart">Cart</Link></li>
+                    <li className={styles.navLi}><Link to="/">Home</Link></li>
+                    <li className={styles.navLi}><Link to="/store">Store</Link></li>
+                    <li className={styles.navLi}><Link to="/cart">Cart</Link></li>
                     <span>{totalQuantity}</span>
                 </ul>
             </nav>
